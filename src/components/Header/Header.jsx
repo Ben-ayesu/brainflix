f1fbeede390b5b8005c63245ac3d8c256a866944
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-04-30 06:05:14 -0700

    add button styling, search

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,25 @@
 import logo from "../../assets/Logo/BrainFlix-logo.svg";
 import "./Header.scss";
+import "../../styles/partials/_global.scss";
 
 const Header = () => {
   return (
     <header className="header">
+      {/* Logo */}
       <a href="google.com/#blank" className="header__logo">
         <img src={logo} alt="BrainFlix Logo" className="header__img" />
       </a>
+      {/* Search box */}
+      <input
+        className="header__input"
+        type="search"
+        id="search"
+        placeholder="Search"
+      />
+      {/* Button */}
+      <button className="header__btn" id="upload">
+        Upload
+      </button>
     </header>
   );
 };
