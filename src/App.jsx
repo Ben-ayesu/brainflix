76effb872780adefa053a90fba17ac61a3049cfd
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-04-30 19:46:28 -0700

    add video player

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import Header from "./components/Header/Header";
+import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
+
 import "./styles/App.scss";
 
 function App() {
   return (
     <>
       <Header></Header>
+      <VideoPlayer></VideoPlayer>
     </>
   );
 }
---
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -0,0 +1,13 @@
+import "./VideoPlayer.scss";
+
+const VideoPlayer = () => {
+  return (
+    <video
+      className="video-player"
+      controls
+      poster="https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg"
+    ></video>
+  );
+};
+
+export default VideoPlayer;
