cd625d42b02441071bbaeb3f63f12b421a46195d
Benjamin Ayesu-Attah <ben.ayesu.attah@gmail.com>
2024-05-02 17:09:49 -0700

    render video data dynamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import videoData from "./data/video-details.json";
 import logo from "./assets/Logo/BrainFlix-logo.svg";
 import avatar from "./assets/Images/Mohan-muruge.jpg";
 import Comments from "./components/Comments/Comments";
@@ -5,11 +7,24 @@ import Header from "./components/Header/Header";
 import VideoPlayer from "./components/VideoPlayer/VideoPlayer";
 import "./styles/App.scss";
 
+console.log(videoData);
+
 function App() {
+  const [video, setVideo] = useState(videoData[0]);
+  const [selectVideo, setSelectVideo] = useState({});
+
+  const handleVideoClick = (id) => {
+    console.log(id);
+    const foundVideo = videoData.find((video) => video.id === id);
+    console.log(foundVideo);
+
+    setSelectVideo(foundVideo);
+  };
+
   return (
     <>
       <Header logo={logo} avatar={avatar}></Header>
-      <VideoPlayer></VideoPlayer>
+      <VideoPlayer video={video}></VideoPlayer>
       <Comments></Comments>
     </>
   );
--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,46 +1,36 @@
 import "./VideoPlayer.scss";
 import "../../styles/App.scss";
 
-const VideoPlayer = () => {
+const VideoPlayer = ({ video }) => {
   return (
-    <div className="video-player">
+    <div id={video.id} className="video-player">
       {/* Video player */}
       <video
         className="video-player__media"
         controls
-        poster="https://unit-3-project-api-0a5620414506.herokuapp.com/images/image0.jpg"
+        poster={video.image}
       ></video>
       {/* Video player content */}
       <div className="video-player__details-wrapper">
-        <h2 className="video-player__title">
-          The Future of Artificial Intelligence
-        </h2>
+        <h2 className="video-player__title">{video.title}</h2>
         {/* Wraps video channel, date, views, likes  */}
         <div className="video-player__metadata">
           <div className="video-player__channel-date">
-            <h3 className="video-player__channel">By Aiden Thompson</h3>
-            <h3 className="video-player__date">8/8/2023</h3>
+            <h3 className="video-player__channel">{video.channel}</h3>
+            <h3 className="video-player__date">{video.timestamp}</h3>
           </div>
           <div className="video-player__views-likes">
             <div className="video-player__views-container">
               <i className="video-player__icon-views"></i>
-              <h3 className="video-player__views">980,544</h3>
+              <h3 className="video-player__views">{video.views}</h3>
             </div>
             <div className="video-player__likes-container">
               <i className="video-player__icon-likes"></i>
-              <h3 className="video-player__likes">22,479</h3>
+              <h3 className="video-player__likes">{video.likes}</h3>
             </div>
           </div>
         </div>
-        <p className="video-player__description">
-          Explore the cutting-edge developments and predictions for Artificial
-          Intelligence in the coming years. From revolutionary breakthroughs in
-          machine learning to the ethical considerations influencing AI
-          advancements, this exploration transcends the boundaries of mere
-          speculation. Join us on a journey that navigates the intricate
-          interplay between innovation, ethics, and the ever-evolving tech
-          frontier.
-        </p>
+        <p className="video-player__description">{video.description}</p>
       </div>
     </div>
   );
